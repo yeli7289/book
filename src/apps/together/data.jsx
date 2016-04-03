be4ceef59bd346d92fafb82dac2286c326b9a21a
sophyzhou <sophyzhou@zoes-MacBook-Pro.local>
2016-04-02 20:25:56 -0600

    add profile image

--- src/apps/together/data.jsx
+++ src/apps/together/data.jsx
@@ -138,7 +138,8 @@ actions.sendMessage = function(message,time){
         messageRef.set({
             username: localStorage.getItem('username'),
             message: message,
-            time: time
+            time: time,
+            id: localStorage.getItem('id')
         })
     } else {
         Materialize.toast('Please enter your message', 3000, 'rounded')
